f201baa2ad64c7ca7a667f5a46cb5c09c1dafc01
Ricardo Almeida <ricardoaimalmeida@gmail.com>
2024-04-21 17:17:48 +0100

    My first action is ready

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const axios = require("axios");
+
+try {
+  // `who-to-greet` input defined in action metadata file
+  const url = core.getInput("url");
+  console.log(`Calling ${url}!`);
+  const time = new Date().toTimeString();
+  core.setOutput("time", time);
+
+  await axios.post(url, {
+    text: "Hello, World!",
+  });
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
